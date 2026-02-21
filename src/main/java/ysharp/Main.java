040bb00f8bef40eeb571c3ba375eb3523c35feaa
yagizerdem <126137803+yagizerdem@users.noreply.github.com>
2026-02-22 02:56:32 +0300

    bug fix in parser

--- src/main/java/ysharp/Main.java
+++ src/main/java/ysharp/Main.java
@@ -1,14 +1,25 @@
 package ysharp;
 
 import ysharp.lexer.Cursor;
+import ysharp.lexer.Lexer;
 import ysharp.lexer.Preprocess;
+import ysharp.parser.Parser;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws  Exception {
 
+        String program = "4 + 3";
+        var buf = Preprocess.removeComments(Preprocess.mergeContinuation(program));
+        Lexer lexer = new Lexer(buf);
+        var stream = lexer.scanTokens();
 
+        Parser parser = new Parser(stream);
+
+        var parseTree = parser.parse();
+
+        int a =10;
 
     }
 }
