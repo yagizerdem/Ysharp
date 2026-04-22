d91e035f706b44813a74156ef102201fe0d8b886
yagizerdem <yagizerdem819@gmail.com>
2026-04-22 13:27:13 +0300

    update web

--- webdoc/docusaurus.config.ts
+++ webdoc/docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://yagizerdem.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/Ysharp/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "yagizerdem", // Usually your GitHub org/user name.
+  projectName: "Ysharp", // Usually your repo name.
 
   onBrokenLinks: "throw",
 
@@ -44,21 +44,21 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ["rss", "atom"],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-          // Useful options to enforce blogging best practices
-          onInlineTags: "warn",
-          onInlineAuthors: "warn",
-          onUntruncatedBlogPosts: "warn",
-        },
+//         blog: {
+//           showReadingTime: true,
+//           feedOptions: {
+//             type: ["rss", "atom"],
+//             xslt: true,
+//           },
+//           // Please change this to your repo.
+//           // Remove this to remove the "edit this page" links.
+//           editUrl:
+//             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+//           // Useful options to enforce blogging best practices
+//           onInlineTags: "warn",
+//           onInlineAuthors: "warn",
+//           onUntruncatedBlogPosts: "warn",
+//         },
         theme: {
           customCss: "./src/css/custom.css",
         },
@@ -85,7 +85,6 @@ const config: Config = {
           position: "left",
           label: "Tutorial",
         },
-        { to: "/blog", label: "Blog", position: "left" },
         {
           href: "https://github.com/yagizerdem/Ysharp",
           label: "GitHub",
@@ -125,10 +124,6 @@ const config: Config = {
         {
           title: "More",
           items: [
-            {
-              label: "Blog",
-              to: "/blog",
-            },
             {
               label: "GitHub",
               href: "https://github.com/yagizerdem/Ysharp",
